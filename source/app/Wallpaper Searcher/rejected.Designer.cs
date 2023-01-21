d1cd98f4ef468ffe3118c3b7eab0bf117e7e64e6
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-01-21 22:20:31 +0900

    Add files via upload

---
+++ source/app/Wallpaper Searcher/rejected.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Wallpaper_Searcher
+{
+    partial class rejected
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rejected));
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonSelectCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonReturn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel.AutoScroll = true;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(42, 0);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.flowLayoutPanel.Size = new System.Drawing.Size(1296, 739);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.buttonReturn);
+            this.panel1.Controls.Add(this.buttonSelectAll);
+            this.panel1.Controls.Add(this.buttonSelectCancel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Location = new System.Drawing.Point(0, 745);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1377, 71);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.Location = new System.Drawing.Point(647, 13);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(143, 47);
+            this.buttonSelectAll.TabIndex = 3;
+            this.buttonSelectAll.Text = "すべて選択";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.ButtonSelectAll_Click);
+            // 
+            // buttonSelectCancel
+            // 
+            this.buttonSelectCancel.Location = new System.Drawing.Point(796, 13);
+            this.buttonSelectCancel.Name = "buttonSelectCancel";
+            this.buttonSelectCancel.Size = new System.Drawing.Size(143, 47);
+            this.buttonSelectCancel.TabIndex = 2;
+            this.buttonSelectCancel.Text = "すべて解除";
+            this.buttonSelectCancel.UseVisualStyleBackColor = true;
+            this.buttonSelectCancel.Click += new System.EventHandler(this.ButtonSelectCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(65, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(543, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "除外リストから解除したい画像を選択してください。";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(966, 13);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(215, 47);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "除外リストから外す";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // buttonReturn
+            // 
+            this.buttonReturn.Location = new System.Drawing.Point(1228, 13);
+            this.buttonReturn.Name = "buttonReturn";
+            this.buttonReturn.Size = new System.Drawing.Size(110, 47);
+            this.buttonReturn.TabIndex = 4;
+            this.buttonReturn.Text = "戻る";
+            this.buttonReturn.UseVisualStyleBackColor = true;
+            this.buttonReturn.Click += new System.EventHandler(this.buttonReturn_Click);
+            // 
+            // rejected
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1377, 817);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "rejected";
+            this.Text = "Wallpaper Searcher - 除外リスト";
+            this.Load += new System.EventHandler(this.rejected_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonSelectAll;
+        private System.Windows.Forms.Button buttonSelectCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonReturn;
+    }
+}
---
+++ source/app/Wallpaper Searcher/rejected.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Wallpaper_Searcher
+{
+    public partial class rejected : Form
+    {
+        public class ProductData
+        {
+            public ProductSetting setting { get; set; }
+            public List<List<string>> words { get; set; }
+            public List<string> rejected { get; set; }
+        }
+
+        public class ProductSetting
+        {
+            public bool visible { get; set; }
+            public int range { get; set; }
+            public int vertical { get; set; }
+            public int horizontal { get; set; }
+            public int min_vertical { get; set; }
+            public int min_horizontal { get; set; }
+        }
+
+
+        public static ProductData data = ReadJson(@"script\data.json"); //data.jsonを読み込み、グローバル変数として定義
+
+        private List<PictureBox> imageList = new List<PictureBox>();
+
+        private List<PictureBox> checkMarkList = new List<PictureBox>();
+
+        private List<bool> imageState = new List<bool>();
+
+        public rejected()
+        {
+            InitializeComponent();
+        }
+
+        private void rejected_Load(object sender, EventArgs e)
+        {
+            AddImage();
+            flowLayoutPanel.VerticalScroll.Visible = true;
+        }
+
+        private static ProductData ReadJson(string path)
+        {
+            StreamReader r = new StreamReader(path, Encoding.UTF8);
+            string jsonStr = r.ReadToEnd();
+            r.Close();
+            ProductData jsonData = JsonSerializer.Deserialize<ProductData>(jsonStr);
+            return jsonData;
+        }
+
+        static void WriteJson(ProductData data, string path)
+        {
+            string jsonStr = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(path, jsonStr);
+        }
+
+        public void AddImage()
+        {
+            // List<PictureBox> imageList = new List<PictureBox>();
+            for (int i = 2; i < data.rejected.Count; i++)
+            {
+                PictureBox picture = new PictureBox();
+                picture.Size = new Size(400, 300);
+                picture.ImageLocation = data.rejected[i];
+                //画像サイズ調整
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+                picture.BorderStyle = BorderStyle.FixedSingle;
+                //関数
+                picture.Tag = i - 2;
+                picture.Click += Picture_Click;
+                //その他
+                picture.BackColor = Color.AliceBlue;
+                picture.Cursor = Cursors.Hand;
+                //レ点(画像)
+                PictureBox checkMark = new PictureBox();
+                checkMark.Size = new Size(100, 100);
+                checkMark.BackColor = Color.Transparent;
+                checkMark.ImageLocation = @"script\checkmark.png";
+                checkMark.SizeMode = PictureBoxSizeMode.Zoom;
+                checkMark.Visible = false;
+                checkMark.Click += CheckMark_Click;
+                checkMark.Tag = i - 1;
+                //要素追加
+                imageList.Add(picture);
+                imageState.Add(false);
+                flowLayoutPanel.Controls.Add(picture);
+
+                picture.Controls.Add(checkMark);
+                checkMarkList.Add(checkMark);
+            }
+        }
+
+        private void Picture_Click(object sender, EventArgs e)
+        {
+            int tagNum = (int)((PictureBox)sender).Tag;
+            imageState[tagNum] = !imageState[tagNum];
+            checkMarkList[tagNum].Visible = !checkMarkList[tagNum].Visible;
+        }
+
+        private void CheckMark_Click(object sender, EventArgs e)
+        {
+            int tagNum = (int)((PictureBox)sender).Tag;
+            imageState[tagNum] = !imageState[tagNum];
+            checkMarkList[tagNum].Visible = !checkMarkList[tagNum].Visible;
+        }
+
+        private void ButtonSelectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < imageState.Count; i++) imageState[i] = true;
+            //foreach (PictureBox image in imageList) image.BackColor = Color.Red;
+            foreach (PictureBox check in checkMarkList) check.Visible = true;
+        }
+
+        private void ButtonSelectCancel_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < imageState.Count; i++) imageState[i] = false;
+            //foreach (PictureBox image in imageList) image.BackColor = Color.AliceBlue;
+            foreach (PictureBox check in checkMarkList) check.Visible = false;
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            for(int i = imageState.Count - 1; i >= 0; i--)
+            {
+                if(imageState[i] == true)
+                {
+                    data.rejected.RemoveAt(i + 2);
+                }
+            }
+            WriteJson(data, @"script\data.json");
+            Close();
+        }
+
+        private void buttonReturn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
